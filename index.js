72db44cdca941b227023280a1cc0dfd054d21994
Lisa Chan <xlisachan@gmail.com>
2018-02-20 18:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + n).parseInt()
+  }
+}

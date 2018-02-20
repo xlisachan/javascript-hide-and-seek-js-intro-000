e7095e13347783046637783ef493799e4a37f607
Lisa Chan <xlisachan@gmail.com>
2018-02-20 18:05:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('app').querySelector('#nested .target');
 }
+
+function deepestChild() {
+  const divs = document.getElementById('app').querySelectorAll('div.grand-node div')
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].innerHTML = (i + 1).toString()
+  }
+}

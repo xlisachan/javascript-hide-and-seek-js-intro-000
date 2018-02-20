906ff3f773aa94d4c5fa96ac756fbc98590d4f10
Lisa Chan <xlisachan@gmail.com>
2018-02-20 18:37:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML + n);
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
   }
 }

dca992387bcdb40f33fb4db2375b6085bd95af59
Lisa Chan <xlisachan@gmail.com>
2018-02-20 18:00:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('div').querySelector('#nested target');
+  return document.getElementById('app').querySelector('#nested target');
 }
 
-function deepestChild() {
-  const deepestChild = document.getElementById('app').querySelectorAll('div grand-node')
-
-  for (let i = 0; i < app.length; i++) {
-    div[i].innerHTML = (i + 1).toString()
-  }
-}

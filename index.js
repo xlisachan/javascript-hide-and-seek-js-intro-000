a28cc7ba327845b2a176cda6db21351040eb7bd2
Lisa Chan <xlisachan@gmail.com>
2018-02-20 18:40:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
   }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
 }
